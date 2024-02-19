5306930636552c67bb38abe70c51f2cc170e8734
Jose Montana <ing.jmontana@gmail.com>
2024-02-19 12:51:28 -0500

    chore(Build): working configuration

---
+++ library/conanfile.py
@@ -0,0 +1,23 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+from conan.errors import ConanInvalidConfiguration
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("zlib/1.2.11")
+        self.requires("gtest/1.14.0")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/3.22.6")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def validate(self):
+        if self.settings.os == "Macos" and self.settings.arch == "armv8":
+            raise ConanInvalidConfiguration("ARM v8 not supported in Macos")
