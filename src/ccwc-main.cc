bac1ba194e4ddcf148c04db93da3df0ebdbc4a32
Jose Montana <ing.jmontana@gmail.com>
2024-03-02 20:26:17 -0500

    feat(CC Count Words): cnofigured program options

--- src/ccwc-main.cc
+++ src/ccwc-main.cc
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <boost/program_options.hpp>
+#include <boost/log/trivial.hpp>
 #include "ccwc.h"
 
-int main()
+namespace po = boost::program_options;
+
+/**
+ * @brief Entry point for the application
+ * @param argc count of parameters passed to the program
+ * @param argv c-string array of the params
+ */
+int main(int argc, char* argv[])
 {
-  ccwc::help();
+  // for (int i = 0; i < argc; i++)
+  // {
+  //   std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+  // }
+  po::options_description desc("Allowed options");
+  desc.add_options()("help,h", "produce help message")("chars,c", "count characters")("words,w", "count words")("lines,l", "count lines")("input-file", po::value<std::string>(), "input file");
+
+  po::positional_options_description p;
+  p.add("input-file", -1);
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+  }
+
+  bool countChars = vm.count("chars") || (!vm.count("words") && !vm.count("lines"));
+  bool countWords = vm.count("words") || (!vm.count("chars") && !vm.count("lines"));
+  bool countLines = vm.count("lines") || (!vm.count("words") && !vm.count("chars"));
+
+  BOOST_LOG_TRIVIAL(debug) << "countChars: " << countChars;
+  BOOST_LOG_TRIVIAL(debug) << "countWords: " << countWords;
+  BOOST_LOG_TRIVIAL(debug) << "countLines: " << countLines;
+
   return EXIT_SUCCESS;
 }
--- src/ccwc.h
+++ src/ccwc.h
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 // wc command line tool for counting characters, words, and lines in a file
 
 #ifndef CCWC_H
@@ -5,7 +9,18 @@
 
 namespace ccwc
 {
-  void help();
+  void count_chars(int& counter);
+  void count_words(int& counter);
+  void count_lines(int& counter);
+
+  class Pipeline {
+  public:
+    void pipe(void (*func)(const std::string&));
+    void process();
+
+  private:
+    std::vector<void (*)(const std::string&)> pipes;
+  };
 }
 
-#endif // CCWC_H
+#endif // 
